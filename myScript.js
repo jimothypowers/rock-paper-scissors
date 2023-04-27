382d6790c17539812ca7940c964ed7e1c7b6dafc
jimothypowers <jimothypowers@gmail.com>
2023-04-26 23:51:24 -0700

    Update myscript.js

--- myScript.js
+++ myScript.js
@@ -1,14 +1,37 @@
+/*function getComputerChoice() {
+  const choices = ['rock', 'paper', 'scissors'];
+  const randomIndex = Math.floor(Math.random() * 3);
+  return choices[randomIndex];
+}
+
+function playRound(playerSelection, computerSelection) {
+  // Make playerSelection case-insensitive
+  playerSelection = playerSelection.toLowerCase();
+  
+  // Determine winner or tie
+  if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  } else if (
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper')
+  ) {
+    return `You win! ${playerSelection} beats ${computerSelection}.`;
+  } else {
+    return `You lose! ${computerSelection} beats ${playerSelection}.`;
+  }
+}
+*/
 function getComputerChoice() {
-    const choices = ['rock', 'paper', 'scissors'];
+    const choices = ['Rock', 'Paper', 'Scissors'];
     const randomIndex = Math.floor(Math.random() * 3);
     return choices[randomIndex];
   }
   
   function playRound(playerSelection, computerSelection) {
-    // Make playerSelection case-insensitive
     playerSelection = playerSelection.toLowerCase();
-    
-    // Determine winner or tie
+    computerSelection = computerSelection.toLowerCase();
+  
     if (playerSelection === computerSelection) {
       return "It's a tie!";
     } else if (
@@ -19,6 +42,32 @@ function getComputerChoice() {
       return `You win! ${playerSelection} beats ${computerSelection}.`;
     } else {
       return `You lose! ${computerSelection} beats ${playerSelection}.`;
+    }
+  }
+  
+  function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    
+    for (let i = 0; i < 5; i++) {
+      const playerSelection = prompt("Rock, paper, or scissors?");
+      const computerSelection = getComputerChoice();
+      const result = playRound(playerSelection, computerSelection);
+      console.log(result);
+      
+      if (result.includes('win')) {
+        playerScore++;
+      } else if (result.includes('lose')) {
+        computerScore++;
+      }
+    }
+  
+    if (playerScore > computerScore) {
+      console.log(`You won the game! Your score: ${playerScore}. Computer score: ${computerScore}.`);
+    } else if (computerScore > playerScore) {
+      console.log(`You lost the game! Your score: ${playerScore}. Computer score: ${computerScore}.`);
+    } else {
+      console.log(`It's a tie! Your score: ${playerScore}. Computer score: ${computerScore}.`);
     }
   }
   
